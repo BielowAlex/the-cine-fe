5d4654e5cd6de7f7d7320660881661c5304f492c
BielowAlex <drohomyretskyi.oleksandr@gmail.com>
2024-05-20 23:23:58 +0200

    feat: update Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import { Hero } from "@/components";
+import TrendingMovies from "@/components/features/TrendingMovies/TrendingMovies";
+import { FC } from "react";
 
-export default function Home() {
+const Home: FC = () => {
   return (
-    <main className="flex flex-col items-center justify-between">
+    <main className="flex flex-col items-center justify-between gap-10">
       <Hero />
+      <TrendingMovies />
     </main>
   );
-}
+};
+
+export default Home;
