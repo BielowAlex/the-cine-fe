db2301dff410300842b4a46fca5fe37da556a7d2
BielowAlex <drohomyretskyi.oleksandr@gmail.com>
2024-05-20 21:38:50 +0200

    feat: add image host

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        remotePatterns:[{hostname: 'thecine.vercel.app'}]
+        remotePatterns:[{hostname: 'thecine.vercel.app'}, {hostname:"image.tmdb.org"}]
     }
 };
 
