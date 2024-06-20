16165d283bfc3009b94e111f36ad78e2168eee6f
BielowAlex <drohomyretskyi.oleksandr@gmail.com>
2024-06-20 13:09:06 +0200

    feat: add host in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        remotePatterns:[{hostname: 'thecine.vercel.app'}, {hostname:"image.tmdb.org"}]
+        remotePatterns:[{hostname: 'thecine.vercel.app'}, {hostname:"image.tmdb.org"}, {hostname:"t3.ftcdn.net"}]
     }
 };
 
